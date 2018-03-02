d7df73a05e78575e948c6aa04a63f2ad1b9955ce
Antonio Díaz Arroyo <antonio.diaz.arroyo@gmail.com>
2018-03-02 11:45:30 +0100

    clase3 ejemplos

---
+++ clase3/ejercicios/01_ejercicio/app.js
@@ -0,0 +1,32 @@
+/* jshint node: true */
+const express= require('express');
+const logger = require('morgan');
+
+
+
+
+var competitions = require('./routes/competitions.js');
+const app = express();
+app.use(logger('dev'));
+app.use('/competitions', competitions);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found!!');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('erroraco!!!!');
+});
+
+
+module.exports = app;
